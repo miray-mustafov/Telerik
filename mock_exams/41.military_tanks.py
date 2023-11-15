8645eb7c2f831edf7bfbac4166cf8ec64a9e513c
miray-mustafov <mustafovmiray@gmail.com>
2023-11-15 12:18:08 +0200

    4  1.military tanks

---
+++ mock_exams/41.military_tanks.py
@@ -0,0 +1,23 @@
+'''
+Military Tanks
+Military scientists are training battle tanks using 
+artificial intelligence. The first lesson is to teach 
+them to move across the (x,y) - plane. They give them a sequence 
+of moves and observe whether the tanks get to the correct (x, y) position 
+on the field. This sequence is represented by string, and the character 
+at position i represents the tank's i-th move.
+'''
+
+# commands = input()
+commands = 'LLRD'
+x, y = 0, 0
+for comm in commands:
+    if comm == 'L':
+        x -= 1
+    if comm == 'R':
+        x += 1
+    if comm == 'U':
+        y += 1
+    if comm == 'D':
+        y -= 1
+print(f"{x}, {y}")
