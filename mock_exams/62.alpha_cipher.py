bb6173b623ba2ed19003eb390e0850dd8a37247e
miray-mustafov <mustafovmiray@gmail.com>
2023-11-19 19:51:30 +0200

    safe house

--- mock_exams/62.alpha_cipher.py
+++ mock_exams/62.alpha_cipher.py
@@ -3,6 +3,5 @@ for _ in range(5):
     num = input()
     product_of_digits = int(num[0]) * int(num[1]) * int(num[2])
     product_of_digits = product_of_digits % 10
-    cipher+=str(product_of_digits)
+    cipher += str(product_of_digits)
 print(cipher)
-
