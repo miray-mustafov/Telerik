b2d25cf8a371c71db2d0f1274fbb4adb7b2c3318
miray-mustafov <mustafovmiray@gmail.com>
2023-11-20 12:07:11 +0200

    6  3.safe houses fixed

--- mock_exams/63.safe_houses.py
+++ mock_exams/63.safe_houses.py
@@ -1,13 +1,15 @@
-import math
+'''
+Safe Houses
+'''
 
 spawns = int(input())
 safe_houses = sorted([int(x) for x in input().split()])
 gr_distance = safe_houses[0]  # Declaring the first distance from the start
-for i in range(1, len(safe_houses)):  # Traversing the distances in berween the safes
-    cur_distance = math.ceil((safe_houses[i] - safe_houses[i - 1]) / 2)
+for i in range(1, len(safe_houses)):  # Traversing the distances in between the safes
+    cur_distance = (safe_houses[i] - safe_houses[i - 1]) // 2
     if gr_distance < cur_distance:
         gr_distance = cur_distance
 
-if gr_distance < spawns - safe_houses[-1]:  # Checking also the last spawn
-    gr_distance = spawns - safe_houses[-1]
+if gr_distance < spawns - 1 - safe_houses[-1]:  # Checking also the last spawn
+    gr_distance = spawns - 1 - safe_houses[-1]
 print(gr_distance)
