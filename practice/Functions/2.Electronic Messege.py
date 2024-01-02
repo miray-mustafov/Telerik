6d290622570c9c50db398398ae1e2603004a227b
miray-mustafov <mustafovmiray@gmail.com>
2024-01-02 20:57:36 +0200

    Electronic messege

---
+++ practice/Functions/2.Electronic Messege.py
@@ -0,0 +1,15 @@
+s = input()
+
+import re
+broken_pattern = r'[^a-zA-Z0-9\s]'
+longest_seq_br_chars = current_seq_br_chars = 0
+for char in s:
+    if char == '.':
+        break
+    if re.match(broken_pattern, char):
+        current_seq_br_chars += 1
+    else:
+        longest_seq_br_chars = max(longest_seq_br_chars, current_seq_br_chars)
+        current_seq_br_chars = 0
+
+print(longest_seq_br_chars)
