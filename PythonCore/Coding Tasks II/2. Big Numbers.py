17d4b77d531f472a70843d0a5efb64a64707a40d
miray-mustafov <mustafovmiray@gmail.com>
2024-01-08 15:41:17 +0200

    Big nums update

--- PythonCore/Coding Tasks II/2. Big Numbers.py
+++ PythonCore/Coding Tasks II/2. Big Numbers.py
@@ -1,3 +1,23 @@
+from itertools import zip_longest
+n1, n2 = map(int, input().split())
+list1 = map(int, input().split())
+list2 = map(int, input().split())
+
+res, carry = [], 0
+for d1, d2 in zip_longest(list1, list2, fillvalue=0):
+    sumed_digit = d1 + d2 + carry
+    if sumed_digit > 9:
+        res.append(sumed_digit % 10)
+        carry = 1
+    else:
+        res.append(sumed_digit)
+        carry = 0
+
+if carry:
+    res.append(carry)
+print(*res)
+
+
 # n1, n2 = map(int, input().split())
 # list1 = list(map(int, input().split()))
 # list2 = list(map(int, input().split()))
@@ -28,19 +48,3 @@
 #
 # print(*res)
 
-from itertools import zip_longest
-
-n1, n2 = map(int, input().split())
-list1 = map(int, input().split())
-list2 = map(int, input().split())
-
-res, carry = [], 0
-for d1, d2 in zip_longest(list1, list2, fillvalue=0):
-    sumed_digit = d1 + d2 + carry
-    if sumed_digit > 9:
-        res.append(sumed_digit % 10)
-        carry = 1
-    else:
-        res.append(sumed_digit)
-        carry = 0
-print(*res)
