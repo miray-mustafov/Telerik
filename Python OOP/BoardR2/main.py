fe815d3f07ea0df4211ebd6c58e13282e49f6d52
miray-mustafov <mustafovmiray@gmail.com>
2024-01-16 23:31:45 +0200

    Board class update

--- Python OOP/BoardR2/main.py
+++ Python OOP/BoardR2/main.py
@@ -9,19 +9,12 @@ def add_days_to_now(d):
 
 
 item = BoardItem('Refactor this mess', add_days_to_now(2))
-item.due_date += timedelta(days=365 * 2)  # two years in the future
-item.title = 'Not that important'
-item.revert_status()
-item.advance_status()
-item.revert_status()
-print(item.history())
+anotherItem = BoardItem('Dont refactor anything',  add_days_to_now(2))
 
-print('\n--------------\n')
+board = Board()
+board.add_item(item)
+board.add_item(anotherItem)
+board.add_item(item) # nothing happens
+board.add_item(item) # nothing happens
 
-anotherItem = BoardItem('Dont refactor anything', add_days_to_now(2))
-anotherItem.advance_status()
-anotherItem.advance_status()
-anotherItem.advance_status()
-anotherItem.advance_status()
-anotherItem.advance_status()
-print(anotherItem.history())
+print(board.count) # 2
