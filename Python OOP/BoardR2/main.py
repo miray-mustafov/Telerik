2de70ba77562893b4dc045e7f8b2d12bd4719cfe
miray-mustafov <mustafovmiray@gmail.com>
2024-01-16 18:21:19 +0200

    BoardItem encapsulated

--- Python OOP/BoardR2/main.py
+++ Python OOP/BoardR2/main.py
@@ -8,6 +8,20 @@ def add_days_to_now(d):
     return date.today() + timedelta(days=d)
 
 
-log = EventLog('An important thing happened')
-print(log.description)
-print(log.info())
+item = BoardItem('Refactor this mess', add_days_to_now(2))
+item.due_date += timedelta(days=365 * 2)  # two years in the future
+item.title = 'Not that important'
+item.revert_status()
+item.advance_status()
+item.revert_status()
+print(item.history())
+
+print('\n--------------\n')
+
+anotherItem = BoardItem('Dont refactor anything', add_days_to_now(2))
+anotherItem.advance_status()
+anotherItem.advance_status()
+anotherItem.advance_status()
+anotherItem.advance_status()
+anotherItem.advance_status()
+print(anotherItem.history())
