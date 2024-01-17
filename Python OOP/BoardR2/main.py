28e9d741e1c27aacbb8d4337d6223b89cf01b9d3
miray-mustafov <mustafovmiray@gmail.com>
2024-01-17 14:33:23 +0200

    a

--- Python OOP/BoardR2/main.py
+++ Python OOP/BoardR2/main.py
@@ -6,15 +6,3 @@ from event_log import EventLog
 
 def add_days_to_now(d):
     return date.today() + timedelta(days=d)
-
-
-item = BoardItem('Refactor this mess', add_days_to_now(2))
-anotherItem = BoardItem('Dont refactor anything',  add_days_to_now(2))
-
-board = Board()
-board.add_item(item)
-board.add_item(anotherItem)
-board.add_item(item) # nothing happens
-board.add_item(item) # nothing happens
-
-print(board.count) # 2
