3c1277cde5d0bc81c81b64a8cfb277a8b2f74fe1
miray-mustafov <mustafovmiray@gmail.com>
2024-01-18 14:32:08 +0200

    application data

--- Python OOP/Workshop1/skeleton/core/application_data.py
+++ Python OOP/Workshop1/skeleton/core/application_data.py
@@ -5,34 +5,48 @@ from models.shopping_cart import ShoppingCart
 
 class ApplicationData:
     def __init__(self):
-        raise NotImplementedError()
+        self._products = []
+        self._categories = []
+        self._shopping_cart = None  # todo
 
     @property
     def products(self):
-        raise NotImplementedError()
+        return self._products
 
     @property
     def categories(self):
-        raise NotImplementedError()
+        return self._categories
 
     @property
     def shopping_cart(self) -> ShoppingCart:
-        raise NotImplementedError()
+        return self._shopping_cart
 
     def find_product_by_name(self, name) -> Product:
-        raise NotImplementedError()
+        for product in self.products:
+            if product.name == name:
+                return product
+        raise ValueError(f'Product {name} does\'nt exist')
 
     def find_category_by_name(self, name) -> Category:
-        raise NotImplementedError()
+        for category in self.categories:
+            if category.name == name:
+                return category
+        raise ValueError(f'Category {name} does\'nt exist')
 
     def create_category(self, name) -> None:
-        raise NotImplementedError()
+        if self.category_exists(name):
+            # raise ValueError(f'Category {name} already exists!')
+            return  # todo
+        self._categories.append(Category(name))
 
     def create_product(self, name, brand, price, gender) -> None:
-        raise NotImplementedError()
+        if self.product_exists(name):
+            # raise ValueError(f'Product {name} already exists!')
+            return
+        self._products.append(Product(name, brand, price, gender))
 
     def category_exists(self, name) -> bool:
-        raise NotImplementedError()
+        return name in [x.name for x in self.categories]
 
     def product_exists(self, name) -> bool:
-        raise NotImplementedError()
+        return name in [x.name for x in self.products]
