8eea6db90296761c52d7266f55caa2acd08f792a
miray-mustafov <mustafovmiray@gmail.com>
2024-02-13 20:19:03 +0200

    list files

--- Python OOP/Files/skeleton/commands/list_files.py
+++ Python OOP/Files/skeleton/commands/list_files.py
@@ -1,4 +1,5 @@
 from commands.base.base_command import BaseCommand
+import os
 
 
 class ListFilesCommand(BaseCommand):
@@ -7,4 +8,15 @@ class ListFilesCommand(BaseCommand):
         super().__init__(params, 1)
 
     def execute(self):
-       raise NotImplementedError()
+        directory_name = self._params[0]
+        directory_path = os.path.join(self.demo_folder_path, directory_name)
+
+        if not os.path.exists(directory_path):
+            return f"Error: Directory '{directory_name}' does not exist."
+
+        files = os.listdir(directory_path)
+        if not files:
+            return f"No files found in directory '{directory_name}'."
+        else:
+            file_list = [f" {i + 1}. {file}" for i, file in enumerate(files)]
+            return f"{directory_name} files:\n" + '\n'.join(file_list)
