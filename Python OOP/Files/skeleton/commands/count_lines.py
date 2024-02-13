4122681b895b3163891c95e58f1c63509b422950
miray-mustafov <mustafovmiray@gmail.com>
2024-02-13 20:30:54 +0200

    lines count

--- Python OOP/Files/skeleton/commands/count_lines.py
+++ Python OOP/Files/skeleton/commands/count_lines.py
@@ -1,5 +1,5 @@
-
 from commands.base.base_command import BaseCommand
+import os
 
 
 class CountLinesCommand(BaseCommand):
@@ -8,4 +8,12 @@ class CountLinesCommand(BaseCommand):
         super().__init__(params, 0)
 
     def execute(self):
-       raise NotImplementedError()
+        demo_folder_path = self.demo_folder_path
+
+        total_lines = 0
+        for root, _, files in os.walk(demo_folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r') as f:
+                    total_lines += sum(1 for _ in f)
+        return f"Total lines in all files in demo_folder: {total_lines}"
