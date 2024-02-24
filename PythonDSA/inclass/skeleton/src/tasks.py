33c2887172da9a474e80574a368ea629ae2bb031
miray-mustafov <mustafovmiray@gmail.com>
2024-02-24 07:37:54 +0200

    inclass DSA start

---
+++ PythonDSA/inclass/skeleton/src/tasks.py
@@ -0,0 +1,27 @@
+from src.common.list_node import ListNode
+from src.common.stack import Stack
+
+# task 1
+
+def find_middle_of_list(head: ListNode):
+    raise NotImplementedError()
+
+# task 2
+
+def merge_sorted_lists(h1: ListNode, h2: ListNode):
+    raise NotImplementedError()
+
+# task 3
+
+def reverse_list(head: ListNode):
+    raise NotImplementedError()
+
+# task 4
+
+def validate_parentheses(expr: str):
+    raise NotImplementedError()
+
+# task 5
+
+def backspace_char(sequence: str):
+    raise NotImplementedError()
