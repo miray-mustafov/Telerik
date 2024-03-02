8d078f49ca59ccdac732b77d79b6e87adeffe91c
miray-mustafov <mustafovmiray@gmail.com>
2024-03-02 20:13:47 +0200

    ount occurrences 2

--- PythonDSA/Recursion/count_occurrences_2.py
+++ PythonDSA/Recursion/count_occurrences_2.py
@@ -4,7 +4,10 @@ num = int(input())
 def f(num):
     if num == 0:
         return 0
-    if num % 10 == 7:
+
+    if num % 10 == 8:
+        if (num // 10) % 10 == 8:
+            return 2 + f(num // 10)
         return 1 + f(num // 10)
     return f(num // 10)
 
