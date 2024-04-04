e0e681856b4fb841911a5aae979ddf3e646f5263
miray-mustafov <mustafovmiray@gmail.com>
2024-04-04 16:18:07 +0300

    Endpoints practice, inclass, using dictianary for storage

--- PythonWEB/WebServicesAndREST/inclass/data.py
+++ PythonWEB/WebServicesAndREST/inclass/data.py
@@ -1,15 +1,63 @@
 from pydantic import BaseModel
+from datetime import date
+
 
 class Product(BaseModel):
-    id: int | None
+    id: int | None = None
     name: str
     description: str
     price: float
 
 
-products = [
-    Product(id=1, name='TV', description='LCD 40 Inch', price = 749.99),
-    Product(id=2, name='Laptop', description='2x2.6 GHz CPU; 6GB RAM; HD Graphics', price = 699.99),
-    Product(id=3, name='Smartphone', description='6.55" HD+, 5G', price = 1349.90),
-    Product(id=4, name='Keyboard', description='Full-size Layout, Mechanical', price = 99.00),
-]
+class Order(BaseModel):
+    id: int | None = None
+    customer: str
+    product_ids: list[int]
+    delivery_date: date
+
+
+products = {
+    1: Product(id=1, name='TV', description='LCD 40 Inch', price=749.99),
+    2: Product(id=2, name='Laptop', description='2x2.6 GHz CPU; 6GB RAM; HD Graphics', price=699.99),
+    3: Product(id=3, name='Smartphone', description='6.55" HD+, 5G', price=1349.90),
+    4: Product(id=4, name='Keyboard', description='Full-size Layout, Mechanical', price=99.00),
+}
+
+orders = {
+    1: Order(
+        id=1,
+        customer='Steven',
+        product_ids=[2, 4],
+        delivery_date=date(2025, 2, 8)
+    ),
+    2: Order(
+        id=2,
+        customer='Alice',
+        product_ids=[1],
+        delivery_date=date(2023, 8, 4)
+    ),
+    3: Order(
+        id=3,
+        customer='Alice',
+        product_ids=[3],
+        delivery_date=date(2023, 8, 4)
+    ),
+}
+
+
+def extended_order_info(order) -> dict:
+    extended_order, cur_products, total = {}, [], 0
+
+    for pid in order.product_ids:
+        cur_products.append(products[pid])
+        total += products[pid].price
+    if total > 125:
+        total *= 1.02
+
+    extended_order['id'] = order.id
+    extended_order['customer'] = order.customer
+    extended_order['products'] = cur_products
+    extended_order['delivery_date'] = order.delivery_date
+    extended_order['order_total'] = total
+
+    return extended_order
--- PythonWEB/WebServicesAndREST/inclass/main.py
+++ PythonWEB/WebServicesAndREST/inclass/main.py
@@ -1,8 +1,12 @@
+import uvicorn
 from fastapi import FastAPI, Response
-from data import Product, products
+from data import Product, Order, products, orders, extended_order_info
 
 app = FastAPI()
 
+
+# if __name__ == "__main__":
+#     uvicorn.run('main:app', host="127.0.0.1", port=8000, reload=True)
 
 @app.get('/')
 def root():
@@ -12,9 +16,9 @@ def root():
 @app.get('/products')
 def get_products(
         sort: str | None = None,
-        search: str | None = None
+        search: str | None = None,
 ):
-    result = products
+    result = list(products.values())
 
     if search:
         result = [p for p in result if (search.lower() in p.name.lower())]
@@ -27,7 +31,7 @@ def get_products(
 
 @app.get('/products/{id}')
 def get_product_by_id(id: int):
-    product = next((p for p in products if p.id == id), None)
+    product = next((p for p in products.values() if p.id == id), None)
 
     if product is None:
         return Response(status_code=404)
@@ -37,23 +41,88 @@ def get_product_by_id(id: int):
 
 @app.post('/products', status_code=201)
 def create_product(product: Product):
-    max_id = max(p.id for p in products)
+    max_id = max(n for n in products)
 
     product.id = max_id + 1
-    products.append(product)
+    products[product.id] = product
 
     return product
 
 
 @app.put('/products/{id}')
 def update_product(id: int, product: Product):
-    existing_product = next((p for p in products if p.id == id), None)
+    existing_product = products.get(id, None)
 
     if existing_product is None:
         return Response(status_code=404)
-    else:
-        existing_product.name = product.name
-        existing_product.description = product.description
-        existing_product.price = product.price
 
-        return existing_product
+    existing_product.name = product.name
+    existing_product.description = product.description
+    existing_product.price = product.price
+
+    return existing_product
+
+
+# ---------------------ORDERS--------------------------------
+@app.get('/orders')
+def get_orders(
+        sort: str | None = None,
+        search: str | None = None,
+):
+    # sorts by delivery_date
+    # searches by customer
+
+    cur_orders = list(orders.values())
+    if search:
+        cur_orders = [o for o in cur_orders if search.lower() in o.customer.lower()]
+    if sort == 'asc' or sort == 'desc':
+        cur_orders = sorted(cur_orders, key=lambda p: p.delivery_date, reverse=sort == 'desc')
+    return cur_orders
+
+
+@app.get('/orders/{id}')
+def get_order_by_id(id: int):
+    order: Order = orders.get(id, None)
+
+    if order is None:
+        return Response(status_code=404)
+    info: dict = extended_order_info(order)
+    return info
+
+
+@app.post('/orders', status_code=201)
+def create_order(order: Order):
+    for pid in order.product_ids:
+        if pid not in products:
+            return Response('Must contain existing products', status_code=400)
+
+    max_id = max(num for num in orders.keys())
+    order.id = max_id + 1
+    orders[order.id] = order
+
+    return order
+
+
+@app.put('/orders/{id}')
+def update_order(id: int, order: Order):
+    existing_order = orders.get(id, None)
+
+    if existing_order is None:
+        return Response('NOT FOUND', status_code=404)
+    for pid in order.product_ids:
+        if pid not in products:
+            return Response('Must contain existing products', status_code=400)
+
+    existing_order.customer = order.customer
+    existing_order.product_ids = order.product_ids
+    existing_order.delivery_date = order.delivery_date
+
+    return extended_order_info(existing_order)
+
+
+@app.delete('/orders/{id}', status_code=204)
+def delete_order(id: int):
+    if id not in orders:
+        return Response('NOT FOUND', status_code=404)
+    orders.pop(id)
+    return f"Order with id {id} deleted!"
