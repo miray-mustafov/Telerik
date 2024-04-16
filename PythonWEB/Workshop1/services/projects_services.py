1b6fac73c1e364c92dbf574a7579b731a59f04fb
miray-mustafov <mustafovmiray@gmail.com>
2024-04-16 19:23:11 +0300

    all() filters

--- PythonWEB/Workshop1/services/projects_services.py
+++ PythonWEB/Workshop1/services/projects_services.py
@@ -3,30 +3,21 @@ from data.database import insert_query, read_query, update_query
 
 
 def all(name=None, limit=None, status=None):
-    if not any([name, limit, status]):
-        query = "SELECT id, name, is_open, team_limit FROM projects"
-    else:
-        query = "SELECT id, name, is_open, team_limit FROM projects WHERE"
+    filters = []
     parameters = []
 
     if name:
-        query += " name LIKE ?"
+        filters.append(f"name LIKE ?")
         parameters.append(f"%{name}%")
-
     if limit is not None:
-        if not name:
-            query += " team_limit <= ?"
-        else:
-            query += " AND team_limit <= ?"
+        filters.append(f"team_limit <= ?")
         parameters.append(limit)
-
     if status:
         is_open = 1 if status.lower() == 'open' else 0
-        if not name and not limit:
-            query += " is_open = ?"
-        else:
-            query += " AND is_open = ?"
+        filters.append(f"is_open = ?")
         parameters.append(is_open)
 
+    query = (f"SELECT id, name, is_open, team_limit FROM projects" +
+             (" WHERE " + " AND ".join(filters) if filters else ""))
     data = read_query(query, parameters)
     return data
