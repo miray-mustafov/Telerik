bc42b29e0749bd8ac9fbea30aa7bf22e9fd10e14
miray-mustafov <mustafovmiray@gmail.com>
2024-05-28 15:51:49 +0300

    example update

--- PythonDSA/interesting_example.py
+++ PythonDSA/interesting_example.py
@@ -1,32 +1,72 @@
+from datetime import datetime
+
 """
-Hi,
-What do you think about the time complexity of second function here?
-At first, it looks like cubic() has O(string^3) but both functions practically scale quadratically,
+Time complexity of cubic function here?
+At first, it looks like cubic() has O(n^3) but both functions practically scale quadratically,
 considering the cases below.
-GPT is confused and gives both answers sometimes.
 """
 
 
-def quadratic(n):  # O(string^2)
-    count = 0
-    for a in range(n):
-        for b in range(n):
-            count += 1
-    print(count)
-
-
-def cubic(n):  # O(?)
+def fake_cubic(n):  # O(?)
     count = 0
     for a in range(n):
         for b in range(n):
             if a == b:
                 for c in range(n):
                     count += 1
-    print(count)
+    return count
 
 
-cubic(20)  # 400 (20*20)
-cubic(30)  # 900 (30*30)
-print()
-quadratic(20)  # 400 (20*20)
-quadratic(30)  # 900 (30*30)
+def linear(n):  # O(n)
+    count = 0
+    for a in range(n):
+        count += 1
+    return count
+
+
+def quadratic(n):  # O(n^2)
+    count = 0
+    for a in range(n):
+        for b in range(n):
+            count += 1
+    return count
+
+
+def quadratic_2(n):  # O(n^2) but effective work complexity is O(n)
+    count = 0
+    for a in range(n):
+        for b in range(n):
+            if a == b:
+                count += 1
+    return count
+
+
+n = 2
+print(linear(n))
+print(quadratic(n))
+print(quadratic_2(n))
+
+# n = 5000
+# print()
+# t1 = datetime.now()
+# res1 = linear(n)
+# t2 = datetime.now()
+# res2 = quadratic(n)
+# t3 = datetime.now()
+# res3 = quadratic_2(n)
+# t4 = datetime.now()
+# print(res1, t2 - t1)
+# print(res2, t3 - t2)
+# print(res3, t4 - t3)
+#
+# print()
+# t1 = datetime.now()
+# res1 = quadratic(5000)
+# t2 = datetime.now()
+# res2 = fake_cubic(5000)
+# t3 = datetime.now()
+# print(res1,t2-t1)
+# print(res2,t3-t2)
+
+# quadratic(2983)  # 8898289
+# fake_cubic(2983)  # 8898289
